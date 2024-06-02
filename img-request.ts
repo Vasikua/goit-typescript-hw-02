ff8f8be42636694da83445f9f85276cc3758000d
Vasyl Mandik <Vasik_ua@ukr.net>
2024-06-02 08:41:19 -0500

    add response

--- img-request.ts
+++ img-request.ts
@@ -1,13 +1,11 @@
+import {Response} from "./src/types"
 import axios from "axios";
 const KEY = "tcc9AyqR4VfB4Dy2jHWCxrETTv4EFWL3mS4oSR5g9gQ";
 axios.defaults.baseURL = "https://api.unsplash.com";
 
-interface Res{
-    total:number;
-    result:string;
-}
 
-export const FetchImages = async (query:string, currentPage:number):Promise<Res> => {
+
+export const FetchImages = async (query:string, currentPage:number):Promise<Response> => {
     const response  = await axios.get("/search/photos?",{
         params: {
             query: query, 
